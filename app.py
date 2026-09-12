80a973694eecadde9c8c520dcb3182f6a4a72b3a
Jhonatan Rocha <118951894+JhonatanSr18@users.noreply.github.com>
2026-09-12 00:32:02 -0300

    Ajustes em alguns pontos do site para a mudança de ambiente

--- app.py
+++ app.py
@@ -32,9 +32,19 @@ app.secret_key = os.environ.get(
 )
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
+database_url = os.environ.get(
+    "DATABASE_URL",
     "sqlite:///showbusiness.db"
 )
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
 app.config[
     "SQLALCHEMY_TRACK_MODIFICATIONS"
